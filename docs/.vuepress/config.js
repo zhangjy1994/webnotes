daf09435ae4ec5810a3936d64991a158703cbacd
zhangjiayu <zhangjiayu@pensees.ai>
2021-03-30 19:37:55 +0800

    feat:新增笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,8 @@ module.exports = {
   description: 'WEB前端工程师',
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '首页', link: '/' },
+      { text: '联系我', href: 'tel:18813023154', link: 'tel:18813023154' },
       { text: 'Guide', link: '/views/' }
     ],
     sidebar: {
@@ -12,14 +13,6 @@ module.exports = {
           title: '前言',
           collapsable: false,
           children: ['']
-        },
-        {
-          title: 'vue',
-          collapsable: true,
-          children: [
-            ['vue/page-a', 'a'],
-            ['vue/page-b', 'b']
-          ]
         },
         {
           title: 'html',
@@ -39,14 +32,27 @@ module.exports = {
           title: 'js',
           collapsable: true,
           children: [
-            ['js/1', '基础知识']
+            ['js/1', '基础知识'],
+            ['js/2', '数据结构']
+          ]
+        },
+        {
+          title: 'vue',
+          collapsable: true,
+          children: [
+            ['vue/1', '知识点'],
+            ['vue/2', 'vue性能优化'],
+            ['vue/page-a', 'md-demo']
           ]
         },
         {
           title: 'interview',
           collapsable: true,
           children: [
-            ['interview/1', '面试题汇总']
+            ['interview/1', '面试题汇总'],
+            ['interview/2', '快手面试题'],
+            ['interview/3', '京东面试题'],
+            ['interview/4', '阿里面试题']
           ]
         },
         {
--- docs/views/README.md
+++ docs/views/README.md
@@ -1,1 +1,5 @@
 ### welcome!
+## 友情链接
+* [曹泽鹏的笔记](https://zhuijing.github.io/)
+* [石志凯的技术杂货铺](https://shikkk.github.io/file/)
+* [忙·南易](https://a.lmongo.com/offer/)
