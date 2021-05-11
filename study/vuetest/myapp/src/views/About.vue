97cc437dd57ac04ae0355e4a6e473dca1c33be57
zhangjiayu <18813023154@163.com>
2021-05-11 20:31:17 +0800

    feat:部分更新

--- study/vuetest/myapp/src/views/About.vue
+++ study/vuetest/myapp/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-for="(item, index) in list" :key="index">
+      <input type="text" v-model="item.name">
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      list: [{ name: 'a' }, { name: 'b' }]
+    }
+  }
+}
+</script>
